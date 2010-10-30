8ebe6e532bc250cb2eaa2fe4605e275d11c10b46
kristoffer.snabb@gmail.com <kristoffer.snabb@gmail.com@577041ae-4a6b-c649-8247-f268630969dd>
2010-10-30 15:58:49 +0000

    pico handler added, the get_rooms function works for the test server,,

---
+++ trunk/battleship/pico.py
@@ -0,0 +1,44 @@
+from xml.sax.xmlreader import IncrementalParser
+from xml.sax.handler import ContentHandler
+import urllib
+
+PICO_URL = "http://localhost:8000/pico"
+
+class pico_feed_handler(ContentHandler):
+    
+    def startDocument():
+        print "start document"
+    
+    def startElement(name, attrs):
+        print "start element"
+        print name
+        print attrs
+        
+    def endElement(name):
+        print "end element"
+        print name
+        
+
+
+def get_rooms():
+    """
+    this function returns the list
+    of the rooms available
+    """
+    con = urllib.urlopen(PICO_URL + "/rooms")
+    return con.read()
+
+def get_room_info(room):
+    """
+    This functions returns the information of the
+    given room
+    """
+    pass
+
+def get_feed(room):
+    """
+    This function returns a http ?! feed
+    for the room given as parameter
+    """
+    pass
+
