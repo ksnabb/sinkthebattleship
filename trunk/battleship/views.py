ebca231d7f4e38c766be7275efa2cefc541895d4
kristoffer.snabb@gmail.com <kristoffer.snabb@gmail.com@577041ae-4a6b-c649-8247-f268630969dd>
2010-11-07 08:39:02 +0000

    the long poll testing works, documentation coming up

--- trunk/battleship/views.py
+++ trunk/battleship/views.py
@@ -2,18 +2,28 @@
 from django.core.cache import cache
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
-
+import json
+import time
 
 def test_page(request):
     return render_to_response('test.html')
     
 def room_clusters(request):
     client_clusters = request.GET
-    print client_clusters
+    if len(client_clusters.keys()) == 0:
+        client_clusters = None
+    else:
+        client_clusters = client_clusters.keys()[0]
+        
     server_clusters = cache.get("danceroom")
+    print client_clusters
     print server_clusters
     
-    #if server_clusters == None:
-    return HttpResponse("wait")
-    #else:
-    #   return HttpResponse(c)
+    while client_clusters == server_clusters:
+        time.sleep(1)
+        server_clusters = cache.get("danceroom")
+        print client_clusters
+        print server_clusters
+    
+    return HttpResponse(server_clusters)
+    
