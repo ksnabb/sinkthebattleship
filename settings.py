8298a8e5116fbf1c07e43c525bcbeb66ce31ada2
kristoffer.snabb@gmail.com <kristoffer.snabb@gmail.com@577041ae-4a6b-c649-8247-f268630969dd>
2010-11-17 19:25:27 +0000

    the json modified to be more like the xml,, not that much information lost

--- settings.py
+++ settings.py
@@ -102,4 +102,4 @@ PICO_URL = "http://127.0.0.1:8080/pico"
 
 CACHE_BACKEND = 'file://' + \
                 os.path.dirname(__file__) + \
-                '/django_cache?timeout=300&max_entries=50'
+                '/django_cache?timeout=10&max_entries=50'
